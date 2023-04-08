6aa5ada4b36ae2b3542627889f2158b88cd5132b
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2023-04-08 14:32:39 +0300

    Updated.

--- 02-Vue-Uygulama/app.js
+++ 02-Vue-Uygulama/app.js
@@ -1,5 +1,28 @@
 const app = Vue.createApp({
-    template: '<h2>Bu ilk uygulamam. </h2>'
+    template: `
+    <h2>Başlık: {{baslik}}</h2>
+    <h3>İçerik: {{icerik}}</h3>
+    <p>Yorum Sayısı: {{yorumSayisi}}</p>
+    <button v-on:click="ekle()">Ekle</button>
+    <p>{{yazi}}<p>
+    `,
+    data() {
+        return {
+            baslik: 'Vue Dersi',
+            icerik: 'Vue 3 Konuları',
+            yorumSayisi: 0,
+            yazi: 'Yorum Eklemek İçin Tıklayın.'
+
+        }
+    },
+    methods: {
+        ekle() {
+            console.log('Ekle Metodu Çalıştı');
+            this.yorumSayisi++;
+            this.yazi = `${this.yorumSayisi} tane yorum girildi.`
+        }
+    },
+
 });
 
 
