a252a60fb9d40c7c2dd6983769ff8d847e28b71d
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2023-04-11 02:18:42 +0300

    Update

---
+++ 12-CompositionAPI/composition-api/src/components/Contents.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <div v-for="content in contents" :key="content.id">
+    {{ content.title }}
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['contents'],
+  setup(props) {
+    console.log(props.contents);
+  },
+}
+</script>
+
+<style>
+
+</style>
--- 12-CompositionAPI/composition-api/src/views/HomeView.vue
+++ 12-CompositionAPI/composition-api/src/views/HomeView.vue
@@ -1,39 +1,25 @@
 <template>
   <div class="home">
-    <input type="text" v-model="search">
-    <div v-for="day in finded" :key="day">
-    {{ day }}
-    </div>
-    <button @click="stop">Stop</button>
+    <Contents :contents="contents"></Contents>
   </div>
 </template>
 
 <script>
 import { ref, computed, watch, watchEffect } from "vue";
-import Content from "../components/Content.vue";
+import Contents from "../components/Contents.vue";
 export default {
   name: 'HomeView',
   components: {
-    
+    Contents
   },
   setup() {
-    const days = ref(['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma']);
-    const search = ref('');
-    const finded = computed(() => {
-      return days.value.filter((day)=>day.includes(search.value))
-    })
-    const watchS = watch(search,()=>{
-      console.log("watch çalıştı")
-    });
-    const watchEffectS = watchEffect(()=> {
-      console.log("watchEffect çalıştı", days.value)
-      console.log("watchEffect çalıştı", search.value)
-    });
-    const stop = () => {
-      watchS();
-      watchEffectS();
-    };
-    return { days, search, finded, stop };
+   const contents = ref([
+   {id:1, title: 'Deneme 1', details: 'Detail 1'},
+   {id:2, title: 'Deneme 2', details: 'Detail 2'},
+   {id:3, title: 'Deneme 3', details: 'Detail 3'},
+   {id:4, title: 'Deneme 4', details: 'Detail 4'},
+   ]);
+   return {contents};
   },
 }
 </script>
