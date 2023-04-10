e06683b6b6beff4b58b1e1dd72f89df74e54cbb4
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2023-04-11 02:39:57 +0300

    Update.

--- 12-CompositionAPI/composition-api/src/components/Contents.vue
+++ 12-CompositionAPI/composition-api/src/components/Contents.vue
@@ -11,24 +11,20 @@
 
 <script>
 import { ref } from 'vue'
+import getContentsFetch from "../composables/getContentsFetch";
+import { useRoute, useRouter } from "vue-router";
 export default {
-  setup(props) {
-    const contents = ref([])
-    const err = ref(null)
-    const getContents = async () => {
-      try {
-        let data = await fetch('https://jsonplaceholder.typicode.com/posts')
-      contents.value = await data.json();
+  setup() {
 
-      if (!data.ok) {
-        throw new Error('Verilere Erişilemedi!');
-      }
-      } catch (error) {
-        err.value = error.message;
-      }
-    }
+    const route = useRoute();
+    const router = useRouter();
+
+    console.log('Route:', route);
+    console.log('Router:', router);
+
+    const { contents, err, getContents } = getContentsFetch()
     getContents();
-    
+
     return {contents, err}
   },
 }
