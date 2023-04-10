049ae3091e2accc61db74cf56f2412ba0909308f
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2023-04-11 02:27:00 +0300

    Update.

--- 12-CompositionAPI/composition-api/src/components/Contents.vue
+++ 12-CompositionAPI/composition-api/src/components/Contents.vue
@@ -3,14 +3,33 @@
   <div v-for="content in contents" :key="content.id">
     {{ content.title }}
   </div>
+  <div v-if="err">
+    {{ err }}
+  </div>
 </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
-  props: ['contents'],
   setup(props) {
-    console.log(props.contents);
+    const contents = ref([])
+    const err = ref(null)
+    const getContents = async () => {
+      try {
+        let data = await fetch('https://jsonplaceholder.typicode.com/posts')
+      contents.value = await data.json();
+
+      if (!data.ok) {
+        throw new Error('Verilere Erişilemedi!');
+      }
+      } catch (error) {
+        err.value = error.message;
+      }
+    }
+    getContents();
+    
+    return {contents, err}
   },
 }
 </script>
